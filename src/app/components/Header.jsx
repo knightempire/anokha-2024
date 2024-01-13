967774b0b807224da7a515bea53f4f83c32bd789
Sajithrajan03 <saisajith03@gmail.com>
2024-01-13 15:32:00 +0530

    Events added

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -32,7 +32,8 @@ const Navbar = () => {
   const [navbarOpen, setNavbarOpen] = useState(false);
 
   return (
-    <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-40 backdrop-blur-md">
+    // {Disabling fixed navbar for now, will enable later - sajith}
+    <nav className="mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-40 backdrop-blur-md">
       <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
         <Link
           href={"/"}
