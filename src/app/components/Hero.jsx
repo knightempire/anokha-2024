e774687f3991365837897e89dcd7dfc8b7c858fc
SuperSecureHuman <supersecurehuman@gmail.com>
2024-01-14 10:58:55 +0530

    Hero Updates

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -1,16 +1,19 @@
 'use client';
 import React from 'react';
 import Image from 'next/image';
-import bg from '/public/images/bg.png';
 import logo from '/public/images/anokha2024_logo.png';
 import DrifterStars from '@devil7softwares/react-drifter-stars';
 
 const Hero = () => (
   <div className="relative h-screen overflow-hidden">
     <DrifterStars
-      color="#ffeed4"
-      motion={{ ratio: 0.015 }}
-      particle={{ count: 60 }} />
+      color="#a1a8b2"
+      motion={{ ratio: 0.015, noiseLength: 1000, noiseStrength: 2 }}
+      particle={{ count: 60 }}
+      links={{fade:300, maxLength:15}}
+      flare={{maxLength:20}}
+      glare={{opacityMultiplier:0.05}}
+       />
     <Image
       src={logo}
       className="absolute bottom-20 left-20 hidden w-1/5 lg:block"
