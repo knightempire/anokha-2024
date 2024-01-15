406e00a4d148d1b2e8ea18ca03a98f999a5b92c1
Sajithrajan03 <saisajith03@gmail.com>
2024-01-16 04:30:31 +0530

    Events Description Added

--- src/app/events/components/EventDescription.jsx
+++ src/app/events/components/EventDescription.jsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import React from "react";
 
-export default function EventCard({
+export default function EventDescription({
   imgSrc,
   eventName,
   eventDesc,
@@ -9,7 +9,7 @@ export default function EventCard({
   price,
 }) {
   return (
-    <div className="flex flex-row bg-white   w-full rounded shadow-lg    ">
+    <div className=" bg-slate-200 h-4/5 w-5/6 z-10 flex ">
       {/* Image Section */}
       <div className="flex-none h-full w-2/4  relative">
         <Image
@@ -22,7 +22,7 @@ export default function EventCard({
         />
       </div>
       {/* Main Content Section */}
-      <div className="flex flex-col hover:bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 transition duration-500 ease-in-out opacity-90">
+      <div className="mt-5 flex flex-col hover:bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 transition duration-500 ease-in-out opacity-90">
         <div className="px-6 py-4">
           <div className="font-bold text-xl mb-2">{eventName}</div>
           <p className="text-gray-700 text-base">{eventDesc}</p>
@@ -42,6 +42,12 @@ export default function EventCard({
         {/* Price Section */}
         <div className="flex items-end justify-end pb-2 pr-8">
           <span className="text-4xl font-bold text-gray-900 ">{`$${price}`}</span>
+        </div>
+
+        <div className="flex justify-center mt-20">
+          <button className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+            Register
+            </button>
         </div>
       </div>
     </div>
--- src/app/events/components/EventLander.jsx
+++ src/app/events/components/EventLander.jsx
@@ -3,10 +3,22 @@
 import React, { useEffect, useState } from "react";
 import EventCard from "@/app/events/components/EventCard";
 import DataHandler from "@/app/events/components/DataHandler";
-
+import EventDescription from "@/app/events/components/EventDescription";
+import { IoCloseCircle } from "react-icons/io5";
 export default function EventLander() {
   const [eventsData, setEventsData] = useState(null);
+  const [isClicked,setIsClicked] = useState(false); //to detect if a event is clicked
+  const [clickedEvent,setClickedEvent] = useState(null)
 
+  const handleEventClick = (curEle)=>{
+      setClickedEvent(curEle)
+      setIsClicked(true)
+      
+    }
+  const handleGoBack = ()=>{
+    setClickedEvent(null)
+    setIsClicked(false)
+  }
   useEffect(() => {
     const fetchData = async () => {
       const eventData = await DataHandler();
@@ -19,11 +31,39 @@ export default function EventLander() {
 
   return (
     <div>
+       
+         <div  >
+          
+          {
+            clickedEvent && isClicked?
+            <div className="absolute flex w-full h-full justify-center mt-5 pt-5 z-10 backdrop-blur-sm   ">
+              
+            <div onClick = {()=>handleGoBack()}  >
+            <IoCloseCircle className="  text-4xl text-black hover:text-red-500 cursor-pointer"/>
+            </div>
+            
+          
+              <EventDescription 
+                key={clickedEvent.id}
+                imgSrc={clickedEvent.imgsrc}
+                eventName={clickedEvent.title}
+                eventDesc={clickedEvent.description}
+                tags={clickedEvent.tags}
+                price={clickedEvent.price}/>
+                </div>
+          :null
+        }
+        </div> 
+        
+      
+      
+
       <div className="flex flex-row min-h-screen mt-5 justify-center items-center mx-10 pt-10 lg:mt-20">
         <div className="grid grid-flow-row gap-8 text-neutral-600 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2   ">
           {eventsData &&
             eventsData.events.map((event) => (
-              <EventCard
+              <div onClick={()=>handleEventClick(event)}>
+              <EventCard 
                 key={event.id}
                 imgSrc={event.imgsrc}
                 eventName={event.title}
@@ -31,6 +71,7 @@ export default function EventLander() {
                 tags={event.tags}
                 price={event.price}
               />
+              </div>
             ))}
         </div>
       </div>
