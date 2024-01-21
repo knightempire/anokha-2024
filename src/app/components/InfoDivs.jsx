2af6d3e5021f4f9570afee7e8a92cafad6ec3ec5
SuperSecureHuman <supersecurehuman@gmail.com>
2024-01-21 19:26:13 +0530

    BG Disco @Info Divs

--- src/app/components/InfoDivs.jsx
+++ src/app/components/InfoDivs.jsx
@@ -1,22 +1,55 @@
 'use client';
-import { useRef } from "react";
+import { useRef, useEffect } from "react";
 import { useTransform, motion, useScroll } from 'framer-motion';
 import Image from "next/image";
 
-const Card = ({ i, title, description, src, url, color, progress, range, targetScale }) => {
+
+const getRandomColorValue = () => {
+  return [Math.random(), Math.random(), Math.random()];
+};
+
+const Card = ({ i, title, description, src, url, color, progress, range, targetScale, onColorChange }) => {
+  const cardRef = useRef(null);
+  useEffect(() => {
+    if (cardRef.current) {
+      const observer = new IntersectionObserver(
+        (entries) => {
+          // Log for debugging purposes:
+          console.log('Observer fired for entry:');
+          console.log(entries[0]);
+
+          if (entries[0].isIntersecting === true) {
+            console.log('Intersection detected. Changing color...');
+            onColorChange({
+              color1: getRandomColorValue(),
+              color2: getRandomColorValue(),
+              color3: getRandomColorValue(),
+            });
+          } else {
+            console.log('Card out of view. Not changing color...');
+          }
+        },
+        {
+          threshold: 0.8,
+        }
+      );
+      observer.observe(cardRef.current);
+
+      return () => observer.unobserve(cardRef.current);
+    }
+  }, []);
 
   const container = useRef(null);
   const { scrollYProgress } = useScroll({
     target: container,
     offset: ['start end', 'start start']
   })
-
   const imageScale = useTransform(scrollYProgress, [0, 1], [2, 1])
   const scale = useTransform(progress, range, [1, targetScale]);
 
   return (
     <div ref={container} className="h-screen flex items-center justify-center sticky overflow-hidden top-10">
-      <motion.div
+      <motion.div ref={cardRef}
         style={{ scale, top: `calc(-5vh + ${i * 25}px)` }}
         className="flex flex-col relative -top-20 h-[75vh] w-[80vw] bg-[rgba(255,255,255,0.05)] backdrop-blur-[10px] border border-[rgba(255,255,255,0.2)] rounded-xl p-12 transform origin-top"
       >
