9379f7b2801bdc6f771b7e2f04035d170c84c56d
Ritesh Koushik <riteshkoushik39@gmail.com>
2024-01-26 17:56:29 +0530

    add Checklist: Filter component build

---
+++ src/app/events/components/filterComponent.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+export default function FilterComponent(){
+    return(
+        <div className="flex">
+            {/* All of the following should be Dropdown-Checkbox Components*/}
+            {/* Select Day: "01" | "02" | "03"; */}
+            <div>
+
+            </div>
+            {/* Select Department: <Bunch of department options> */}
+            <div>
+
+            </div>
+            {/* Select Participants: "Group" | "Individual" */}
+            <div>
+
+            </div>
+            {/* Select Type: "Tech Workshop"| "Non-Tech Workshop" | "Tech Event" | "Non-Tech Event" | "Tech Contest" | "Non-Tech Contest" */}
+            <div>
+
+            </div>
+            {/* Select Tags: <Bunch of tag options> */}
+            <div>
+
+            </div>
+            {/* Select Status: "Registered" | "Not Registered" */}
+            <div>
+
+            </div>
+        </div> 
+    );
+}
