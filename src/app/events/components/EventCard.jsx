6e98207da8bd70b89b20e190b6782050f0d99d21
Thanus Kumaar <tanuskumaara@gmail.com>
2024-01-26 23:13:47 +0530

    Editing the design of event cards and added hover effect for cards

--- src/app/events/components/EventCard.jsx
+++ src/app/events/components/EventCard.jsx
@@ -14,41 +14,62 @@ export default function EventCard({
   isAllowed,
 }) {
   return (
-    <div className="flex flex-row bg-white w-full rounded shadow-lg hover:scale-105 cursor-pointer transition-transform duration-100 ease-in-out">
+    <div className="flex flex-col bg-white w-[250px] rounded shadow-lg hover:scale-105 cursor-pointer transition-transform duration-100 ease-in-out">
       {/* Image Section */}
-      <div className="flex-none h-[300px] w-1/3 relative border">
+      <div className="flex-none h-[350px] w-full relative border group overflow-hidden">
         <Image
           src={imgSrc}
           alt="Event Image"
           layout="fill"
-          objectFit="conta"
+          objectFit="contain"
           objectPosition="center"
-          className="rounded"
+          className="rounded transition-transform transform-gpu duration-300 ease-in-out group-hover:filter group-hover:brightness-[20%]"
         />
+        <div className="absolute inset-0 opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-100 text-white text-center p-7 pt-12">
+          {eventDesc}
+        </div>
       </div>
       {/* Main Content Section */}
       <div className="flex flex-col">
         <div className="px-6 py-4">
           <div className="font-bold text-xl mb-2">{eventName}</div>
-          <p className="text-gray-700 text-base">{date}</p>
-          <p className="text-gray-700 text-base">{eventBlurb}</p>
+          <div className="flex flex-row justify-between">
+            <p className="text-gray-700 text-sm">{date.slice(0,10)}</p>
+            <span className="text-sm font-bold text-gray-900 ">{`$${price}`}</span>
+          </div>
         </div>
         {/* Tags Section */}
-        <div className="px-6 pt-4">
+        <div className="px-3 pb-2 flex flex-row flex-wrap">
           {tags &&
             tags.map((tag, index) => (
               <span
                 key={index}
-                className="inline-block bg-gray-400 rounded-full px-3 py-1 text-sm font-semibold text-gray-900 mr-2 mb-2"
+                className="inline-block bg-gray-400 rounded-full px-3 py-1 text-[10px] font-semibold text-gray-900 mr-2 mb-2 h-full"
               >
                 #{tag.tagName}{" "}
               </span>
             ))}
+          <span className="inline-block bg-gray-400 rounded-full px-3 py-1 text-[10px] font-semibold text-gray-900 mr-2 mb-2">
+            #Hello
+          </span>
+          <span className="inline-block bg-gray-400 rounded-full px-3 py-1 text-[10px] font-semibold text-gray-900 mr-2 mb-2">
+            #Hello
+          </span>
+          <span className="inline-block bg-gray-400 rounded-full px-3 py-1 text-[10px] font-semibold text-gray-900 mr-2 mb-2">
+            #Hello
+          </span>
+          <span className="inline-block bg-gray-400 rounded-full px-3 py-1 text-[10px] font-semibold text-gray-900 mr-2 mb-2">
+            #Hello
+          </span>
+          <span className="inline-block bg-gray-400 rounded-full px-3 py-1 text-[10px] font-semibold text-gray-900 mr-2 mb-2">
+            #Hello
+          </span>
+          <span className="inline-block bg-gray-400 rounded-full px-3 py-1 text-[10px] font-semibold text-gray-900 mr-2 mb-2">
+            #Hello
+          </span>
         </div>
+        
         {/* Price Section */}
-        <div className="flex items-end justify-end pb-2 pr-8">
-          <span className="text-4xl font-bold text-gray-900 ">{`$${price}`}</span>
-        </div>
       </div>
     </div>
   );
