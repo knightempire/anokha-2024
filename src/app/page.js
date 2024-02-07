57f7ac7c39c8f6f22e3c84a6213e26bf7aded172
Tharun Kumarr A <tharunkumarra@gmail.com>
2024-02-07 21:33:10 +0530

    added new info div

--- src/app/page.js
+++ src/app/page.js
@@ -38,86 +38,70 @@ export default function Home() {
   });
   const [cardColor, setCardColor] = useState(webGLColors);
 
-  const getRandomColorValue = () => {
-    let r = Math.random();
-    let g = Math.random();
-    let b = Math.random();
-    return [r, g, b];
-  };
+  // const getRandomColorValue = () => {
+  //   let r = Math.random();
+  //   let g = Math.random();
+  //   let b = Math.random();
+  //   return [r, g, b];
+  // };
 
-  const handleClick = () => {
-    let color1 = getRandomColorValue();
-    for (let i = 0; i < 3; i++) {
-      color1[i] = color1[i] * 255;
-    }
+  // const handleClick = () => {
+  //   let color1 = getRandomColorValue();
+  //   for (let i = 0; i < 3; i++) {
+  //     color1[i] = color1[i] * 255;
+  //   }
 
-    const hexColor = tinycolor({
-      r: color1[0],
-      g: color1[1],
-      b: color1[2],
-    }).toHex();
-    const baseTinycolor = tinycolor(hexColor);
-    const analogousColors = baseTinycolor.monochromatic(2);
+  //   const hexColor = tinycolor({
+  //     r: color1[0],
+  //     g: color1[1],
+  //     b: color1[2],
+  //   }).toHex();
+  //   const baseTinycolor = tinycolor(hexColor);
+  //   const analogousColors = baseTinycolor.monochromatic(2);
 
-    console.log(analogousColors);
-    let color2 = [
-      analogousColors[0]._r,
-      analogousColors[0]._g,
-      analogousColors[0]._b,
-    ];
-    let color3 = [
-      analogousColors[1]._r,
-      analogousColors[1]._g,
-      analogousColors[1]._b,
-    ];
+  //   console.log(analogousColors);
+  //   let color2 = [
+  //     analogousColors[0]._r,
+  //     analogousColors[0]._g,
+  //     analogousColors[0]._b,
+  //   ];
+  //   let color3 = [
+  //     analogousColors[1]._r,
+  //     analogousColors[1]._g,
+  //     analogousColors[1]._b,
+  //   ];
 
-    for (let i = 0; i < 3; i++) {
-      color1[i] = color1[i] / 255;
-      color2[i] = color2[i] / 255;
-      color3[i] = color3[i] / 255;
-    }
+  //   for (let i = 0; i < 3; i++) {
+  //     color1[i] = color1[i] / 255;
+  //     color2[i] = color2[i] / 255;
+  //     color3[i] = color3[i] / 255;
+  //   }
 
-    const randomColors = {
-      color1: color1,
-      color2: color2,
-      color3: color3,
-    };
+  //   const randomColors = {
+  //     color1: color1,
+  //     color2: color2,
+  //     color3: color3,
+  //   };
 
-    setWebGLColors(randomColors);
-    handleCardColorChange(randomColors);
-  };
-  const handleCardColorChange = (color) => {
-    console.log(color);
-    setCardColor(color);
-  };
+  //   setWebGLColors(randomColors);
+  //   handleCardColorChange(randomColors);
+  // };
+  // const handleCardColorChange = (color) => {
+  //   console.log(color);
+  //   setCardColor(color);
+  // };
 
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
-      <button
-        className="fixed right-4 bottom-4 bg-gray-600 hover:bg-gray-700 text-white py-2 px-4 rounded focus:outline-none focus:ring-2 focus:ring-gray-400 z-10"
-        onClick={handleClick}
-      >
-        Change Colors
-      </button>
-      {/* <WebGLApp colors={cardColor} /> */}
+      <WebGLApp colors={cardColor} />
       <Navbar login={true} />
       <Hero className="z-10" />
       <SponsorsMarquee />
-      {projects.map((project, i) => {
-        const targetScale = 1 - (projects.length - i) * 0.05;
-        return (
-          <Info
-            key={`p_${i}`}
-            i={i}
-            {...project}
-            progress={scrollYProgress}
-            range={[i * 0.25, 1]}
-            targetScale={targetScale}
-            onColorChange={handleCardColorChange}
-          />
-        );
-      })}
-      <Gallery />
+      <Info
+        title="About Amrita"
+        src="https://i.imgur.com/LqLc1zR.jpg"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Sed euismod, quam id aliquet ultricies, nunc nisl tincidunt nunc, ac tincidunt justo nisl id nunc. Sed euismod, quam id aliquet ultricies, nunc nisl tincidunt nunc, ac tincidunt justo nisl id nunc. Sed euismod, quam id aliquet ultricies, nunc nisl tincidunt nunc, ac tincidunt justo nisl id nunc. Sed euismod, quam id aliquet ultricies, nunc nisl tincidunt nunc, ac tincidunt"
+      />
       <AnokhaMarquee />
       <Footer current_page="home" />{" "}
       {/* current_page is a prop that is used to highlight the current page in the footer. Possible values are home, team, contact, privacy policy} */}
