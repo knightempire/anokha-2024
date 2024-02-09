0197901318b71679d2149b1009527d8d78ec6e5c
amri-tah <amrithanandini2003@gmail.com>
2024-02-09 22:39:52 +0530

    themes tab trial

--- src/app/hackathon/_components/About.jsx
+++ src/app/hackathon/_components/About.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const About = () => {
   return (
     <div className='h-full w-full bg-[#0A113A] relative overflow-hidden'>
-      <div className="absolute inset-0 bg-gradient-to-r from-cyan-500 to-blue-500 md:w-[80%] sm:w-[70%] p-8 rounded-[20%] left-[1%] blur-3xl levitate"></div>
+      <div className="absolute inset-0 bg-gradient-to-r from-cyan-500 to-blue-500 md:w-[70%] sm:w-[70%] p-8 rounded-[20%] left-[1%] blur-3xl levitate"></div>
       <div className="font-poppins mx-auto py-20 md:px-[20%] sm:px-[10%] text-black relative z-10">
-        <div className='bg-white rounded-md p-5'>
+        <div className='bg-white rounded-md pb-[60px] pt-[40px] px-10'>
           <p className='text-[3rem] font-bold'>About</p>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
--- src/app/hackathon/_components/Themes.jsx
+++ src/app/hackathon/_components/Themes.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import theme1 from '@/../public/images/theme1.png'
 import Image from 'next/image'
+import {Box, Tab} from '@mui/material'
+import {TabContext, TabList, TabPanel} from '@mui/lab'
+import FavoriteIcon from '@mui/icons-material/Favorite'
 
 const Themes = () => {
+  const [value, setValue] = useState('1')
+  const handleChange = (event, newValue)=>{
+    setValue(newValue)
+  }
   return (
     <div className='font-poppins h-[100%] mt-[50px]'>
-      <p className='font-bold text-[3rem]'>Themes</p>
-      {/*<div className='font-bold w-full text-[3rem] ml-[250px] mt-10'>Themes</div>*/}
-      <div className='bg-indigo-500 md:w-[40%] sm:w-[90%] mx-auto  rounded-[15px] my-2'>
+      <p className='font-bold text-[3rem] text-center mb-[20px]'>Themes</p>
+      
+      {/* <div className='bg-indigo-500 md:w-[40%] sm:w-[90%] mx-auto  rounded-[15px] my-2'>
         <Image src={theme1} className='h-[250px] object-cover w-full rounded-t-[15px]'/>
 
         <div className='text-white text-[2.5rem] text-left font-bold pb-7 mx-10 mt-[-30px]'>
@@ -16,8 +23,24 @@ const Themes = () => {
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br>
             </br> Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </div>
-          </div>    
-    </div>
+          </div> 
+          </div>    */}
+          <Box>
+            <TabContext value={value}>
+              <Box sx={{border:1, borderColor:'divider', borderRadius:3}} className='md:w-[50%] sm:w-[80%] mx-auto'>
+                <TabList onChange={handleChange} centered>
+                  <Tab label="Theme one" value="1"/>
+                  <Tab label="Theme two" value="2"/>
+                  <Tab label="Theme three" value="3"/>
+                </TabList>
+
+              </Box>
+              <TabPanel value='1' className='text-center'>Theme 1 Content</TabPanel>
+              <TabPanel value='2' className='text-center'>Theme 2 Content</TabPanel>
+              <TabPanel value='3' className='text-center'>Theme 3 Content</TabPanel>
+
+            </TabContext>
+          </Box>
 
   </div>
   )
