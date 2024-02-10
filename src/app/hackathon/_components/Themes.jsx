0fc33381420e7a92b4980a78da1150d82220995c
Sajithrajan03 <saisajith03@gmail.com>
2024-02-11 03:29:32 +0530

    merge req

--- src/app/hackathon/_components/Themes.jsx
+++ src/app/hackathon/_components/Themes.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Image from "next/image";
-import Glow from "./Glow";
+ 
 const Themes = () => {
   const [selectedTab, setSelectedTab] = useState(0);
   // const [selectedTab1, setSelectedTab1] = useState(0);
