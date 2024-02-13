487ec99c964089cc8e13a681ea8a351a280ed536
amri-tah <amrithanandini2003@gmail.com>
2024-02-13 23:22:03 +0530

    responsive register, round1 and instruction pages

--- src/app/hackathon/instructions/page.js
+++ src/app/hackathon/instructions/page.js
@@ -17,11 +17,9 @@ const Home = () => {
       {/* <Instructions/> */}
     <div className="w-full h-screen  relative flex flex-col justify-center items-center bg-[#0A113A]">
     <div className="font-poppins text-white mx-auto text-center">
-        <h1 className="text-4xl font-bold mb-4">INSTRUCTIONS</h1>
+        <h1 className="text-4xl font-black mb-2">INSTRUCTIONS</h1>
         <p className="text-lg mb-8">Follow these steps to get started</p>
-        <div className="bg-gradient-to-r from-cyan-500 to-blue-500 md:w-[50%] p-8 rounded-[50%] lg:h-[60%] md:h-[40%] sm:h-[25%] lg:left-[25%] sm:left-[12%] md:left-[23%] lg:top-[20%] md:top-[30%] sm:top-[35%] absolute blur-3xl levitate sm:w-[80%]"></div>
-
-
+        <div className="bg-gradient-to-r from-cyan-500 to-blue-500 md:w-[50%] p-8 rounded-[50%] lg:h-[60%] md:h-[30%] sm:h-[25%] lg:left-[25%] sm:left-[12%] md:left-[23%] lg:top-[20%] md:top-[60%] sm:top-[35%] absolute blur-3xl levitate sm:w-[80%]"></div>
                 <SimpleCard  />
 
 
@@ -32,10 +30,10 @@ const Home = () => {
 
     </div>
     <Image src={flowChart} width={1050} alt="intel flowchart" className="mx-auto" />
-    <div class="card ml-auto mr-auto mt-[7%] mb-[7%]">
+    <div class="card mx-auto my-[7%]">
               <div className="bg-gradient-to-r from-gray-500 to-black-500 md:w-[50%] p-8 rounded-[50%] lg:h-[60%] md:h-[40%] sm:h-[25%] lg:left-[25%] sm:left-[12%] md:left-[23%] lg:top-[10%] md:top-[30%] sm:top-[40%] absolute blur-3xl levitate sm:w-[80%] lg:w-[100%]"></div>
               <div class="info relative bg-opacity-60 bg-white bg-blur">
-                <h3 class="text-xl mb-[2%] ml-[25%]">Download Sample PPT</h3>
+                <h3 class="text-lg text-center">Download Sample PPT</h3>
                 <a
                 href="https://amritavishwavidyapeetham-my.sharepoint.com/:p:/g/personal/cb_en_u4cse21608_cb_students_amrita_edu/EaAaxjKyWIVLsBlk1ZIgHHABXoTydcJakpTu6g0Tp3ZXug"
                 download
