a4037bf3c51d7769c2fbff67bb0d0b529ad2f5bc
Sajith Rajan P <93327106+Sajithrajan03@users.noreply.github.com>
2024-02-14 19:00:30 +0530

    Delete src/app/hackathon/DashBoard/page.js

--- src/app/hackathon/DashBoard/page.js
+++
@@ -1,9 +0,0 @@
-"use client";
-import React from "react";
-import DashBoard from "./DashBoard";
-import { useRouter } from "next/navigation";
-
-export default function Page() {
-  const router = useRouter();
-  return <DashBoard router={router} />;
-}
