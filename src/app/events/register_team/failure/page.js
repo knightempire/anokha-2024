cda353e5255822637ac41dd3f2249952f00276e0
Tharun Kumarr A <tharunkumarra@gmail.com>
2024-02-20 23:49:39 +0530

    fixed minor bug in failure responsiveness

--- src/app/events/register_team/failure/page.js
+++ src/app/events/register_team/failure/page.js
@@ -25,9 +25,9 @@ export default function PaymentFailure() {
   return (
     <main className="flex min-h-screen flex-col bg-[#192032]">
       <WebGLApp colors={webGLColors} className="-z-10" />
-      <div className="text-[#04002a] items-center justify-center mx-auto my-auto bg-white rounded-3xl px-8 py-8 z-10 sm:scale-75 md:scale-90 lg:scale-100 xl:scale-100 2xl:scale-110">
+      <div className="text-[#04002a] items-center justify-center my-auto md:mx-auto bg-white rounded-3xl px-8 py-8 z-10 sm:scale-75 md:scale-90 lg:scale-100 xl:scale-100 2xl:scale-110">
         <>
-          <div className="flex items-center justify-center px-32 py-8">
+          <div className="flex items-center justify-center px-28 py-8">
             <Lottie options={defaultOptions} height={150} width={150} />
           </div>
         </>
