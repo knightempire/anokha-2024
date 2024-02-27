89f55d1171a5820b9ae4d33c5b6fa366624b82bb
Thanus Kumaar <thanuskumaara@gmail.com>
2024-02-27 23:58:13 +0530

    star events bug fixed

--- src/app/events/components/EventCard.jsx
+++ src/app/events/components/EventCard.jsx
@@ -154,7 +154,14 @@ export default function EventCard({
                 stroke-linejoin="round"
                 stroke-width="2"
                 d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                fill={starred == 0 ? "none" : "currentColor"}
+                fill={
+                  starred == 0 &&
+                  (isRegistered == 0 ||
+                    isRegistered == undefined ||
+                    isRegistered == null)
+                    ? "none"
+                    : "currentColor"
+                }
               />
             </svg>
           </button>
