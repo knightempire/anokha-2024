5a661602ba5e00c4fcd054648da8e09496da5dc1
Tharun Kumarr A <tharunkumarra@gmail.com>
2024-02-28 11:30:50 +0530

    changed font in hero

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -9,10 +9,10 @@ const Hero = () => (
         <h3 className="text-3xl hero-font font-bold tracking-wider md:text-3xl lg:text-4xl xl:text-5xl text-white">
           #Dare2BeDifferent
         </h3>
-        <h2 className="text-6xl font-spincycle tracking-widest md:text-7xl lg:text-8xl xl:text-9xl font-semibold mt-6 text-orange-500 font-family: Spin-cycle">
-          anokha
+        <h2 className="text-6xl hero-font-2 tracking-widest md:text-7xl lg:text-8xl xl:text-9xl font-semibold mt-6 text-orange-500 font-family: Spin-cycle">
+          Anokha
         </h2>
-        <h2 className="text-6xl font-spincycle tracking-widest md:text-7xl lg:text-8xl xl:text-9xl font-semibold text-orange-500 font-family: Spin-cycle">
+        <h2 className="text-6xl hero-font-2 tracking-widest md:text-7xl lg:text-8xl xl:text-9xl font-semibold text-orange-500 font-family: Spin-cycle">
           2024
         </h2>
         <h2 className="text-6xl md:text-7xl lg:text-8xl xl:text-9xl font-extrabold my-6 text-white"></h2>
