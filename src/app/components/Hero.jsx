61231be9361848b143d871f7fbb680187a319754
Tharun Kumarr A <tharunkumarra@gmail.com>
2024-02-29 19:11:05 +0530

    added dates in anokha hero

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -16,6 +16,12 @@ const Hero = () => (
           2024
         </h2>
         <h2 className="text-6xl md:text-7xl lg:text-8xl xl:text-9xl font-extrabold my-6 text-white"></h2>
+        <h3 className="text-3xl font-roobert tracking-wider md:text-2xl lg:text-3xl xl:text-4xl text-white">
+          12th Edition
+        </h3>
+        <h4 className="text-3xl font-rooberto tracking-wider md:text-xl lg:text-2xl xl:text-3xl text-gray-300 mt-4">
+          April 4th, 5th & 6th
+        </h4>
       </section>
     </div>
     <div className="text-2xl sm:m-5 md:m-10 lg:m-20 xl:m-20 z-10 text-white cursor-default">
