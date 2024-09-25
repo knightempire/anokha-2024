801dde0d689a977430be26df0432f2be5d026216
Ashrockzzz2003 <ashrockzzz2003@gmail.com>
2024-09-25 23:19:24 +0530

    Open registrations for testing.

--- src/app/events/[eventId]/page.js
+++ src/app/events/[eventId]/page.js
@@ -447,7 +447,7 @@ const Event = () => {
                     ? getPayUForm()
                     : setpopupvisibility(true);
                 }}
-                disabled={true}
+                disabled={false}
               >
                 {secureLocalStorage.getItem("isLoggedIn") == "0" ||
                 secureLocalStorage.getItem("isLoggedIn") == undefined ||
