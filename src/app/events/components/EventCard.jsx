bb1c5ebcdefce3b46f230369f05342d014835a4e
Abhinav R <76396917+Abhinav-ark@users.noreply.github.com>
2024-10-10 01:34:36 +0530

    Image overflow bug fixed.

--- src/app/events/components/EventCard.jsx
+++ src/app/events/components/EventCard.jsx
@@ -112,7 +112,7 @@ export default function EventCard({
             width={400}
             height={400}
             alt="https://i.imgur.com/iQy8GLM.jpg"
-            className=" rounded-2xl h-full object-contain transition duration-300 hover:filter hover:brightness-0 hover:grayscale-100 hover:opacity-0"
+            className=" rounded-2xl h-full object-fit transition duration-300 hover:filter hover:brightness-0 hover:grayscale-100 hover:opacity-0"
           />
           {secureLocalStorage.getItem("isLoggedIn") && isRegistered == "1" ? (
             <div class="absolute top-2 left-2 p-1 text-xs text-green-800 font-semibold border border-white rounded-lg bg-green-100">
