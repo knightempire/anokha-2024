74f817812f5b3d9514cae2aad1a9a9dabec25cc1
Abhinav R <76396917+Abhinav-ark@users.noreply.github.com>
2024-10-10 09:09:47 +0530

    added all icons to metaData.

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,7 +7,9 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: `Anokha 2024`,
   description: `12th Annual Techfest of Amrita Viswa Vidyapeetham, Coimbatore.`,
-  favicon: `./favicon.ico`
+  favicon: `./favicon.ico`,
+  icon: `./favicon.ico`,
+  appleIcon: `./favicon.ico`
 }
 
 export default function RootLayout({ children }) {
